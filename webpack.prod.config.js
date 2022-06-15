23b44428a61ac78d9188658034e26ec013cc8be2
Andrey Golovin <ag.expert.it@gmail.com>
2022-06-15 15:53:51 +0300

    2022-06-15 - 2

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,29 +2,14 @@ const path = require('path');
 const miniCSS = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.ts',
-    devtool: 'inline-source-map',
     module : {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: '/node_modules/',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i, 
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 100,
-                        name: 'assets/fonts/[name].[ext]',
-                    }
-                }],
-                exclude: [
-                    '/node_modules/',
-                    '/.vscode/',
-                ],
             },
             {
                 test: /\.s[ac]ss?$/i,
@@ -34,10 +19,24 @@ module.exports = {
                     'sass-loader',
                 ],
                 exclude: '/node_modules/',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        limit: 100,
+                        name: 'assets/fonts/[name].[ext]',
+                    }
+                }],
+                exclude: [
+                    '/node_modules/',
+                    '/.vscode/',
+                ],
             }
         ],
     },
-     plugins: [
+    plugins: [
         new miniCSS({
             filename: './styles/style.css',
         }),
@@ -47,6 +46,6 @@ module.exports = {
     },
     output: {
         filename: 'js/bundle.js',
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname,'prod'),
     },
 };
